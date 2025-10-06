75fa6fad837129844e35ad1d27a0ca5656edfcbc
Rikard <rikardengstrom89@gmail.com>
2025-10-06 09:26:56 +0200

    Added initial porject setup for backend.

---
+++ src/main/java/com/catchapp/api/CatchAppApplication.java
@@ -0,0 +1,9 @@
+package com.catchapp.api;
+
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class CatchAppApplication extends Application {
+}
