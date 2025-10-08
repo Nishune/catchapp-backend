ea5fdc9e699978ba080f70bb7d9420620b62fde1
Rikard <rikardengstrom89@gmail.com>
2025-10-08 21:12:00 +0200

    Added JpaUserRepository and sql querys

--- src/main/java/com/catchapp/repository/jpa/JpaUserRepository.java
+++ src/main/java/com/catchapp/repository/jpa/JpaUserRepository.java
@@ -1,4 +1,47 @@
 package com.catchapp.repository.jpa;
 
-public class JpaUserRepository {
+import com.catchapp.model.User;
+import com.catchapp.repository.UserRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Alternative;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+import java.util.Optional;
+
+@ApplicationScoped
+@Alternative
+public class JpaUserRepository implements UserRepository {
+
+    @PersistenceContext(unitName = "catchappPU")
+    EntityManager em;
+
+    @Override
+    public Optional<User> findByUsername(String username) {
+        var list = em.createQuery("SELECT u FROM User u WHERE LOWER(u.username) = LOWER(:u)", User.class)
+                .setParameter("u", username)
+                .setMaxResults(1)
+                .getResultList();
+        return list.stream().findFirst();
+    }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        var list = em.createQuery("SELECT u FROM User u WHERE LOWER(u.email) = LOWER(:e)", User.class)
+                .setParameter("e", email)
+                .setMaxResults(1)
+                .getResultList();
+        return list.stream().findFirst();
+    }
+
+    @Override
+    public User save(User user) {
+        if (user.getId() == null) {
+            em.persist(user);
+            return user;
+        } else {
+            return em.merge(user);
+        }
+    }
+
 }
