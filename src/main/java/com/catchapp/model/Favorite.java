8bfe38e8792c781e7dfbe0d39129a45e2ae1111e
Rikard <rikardengstrom89@gmail.com>
2025-10-08 09:25:25 +0200

    Started wokring on Favorite model

---
+++ src/main/java/com/catchapp/model/Favorite.java
@@ -0,0 +1,10 @@
+package com.catchapp.model;
+
+import java.time.Instant;
+
+public class Favorite {
+    private long id;
+    private Instant createdAt;
+    private User user;
+    private Activity activity;
+}
